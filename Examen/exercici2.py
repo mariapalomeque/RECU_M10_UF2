6c255af2c9b3bd03043369cfa425043d79ba8aa8
Maria Palomeque Guerrero (ext) <maria.palomeque.external@eviden.com>
2024-06-12 16:30:48 +0200

    Exercici 2

---
+++ Examen/exercici2.py
@@ -0,0 +1,21 @@
+import
+
+
+
+
+sql =
+
+
+query = (CREATE TABLE Alumnes{
+    ID = PRIMARY KEY AUTOINCREMENT,
+    nombre = VARCHAR(255) NOT NULL,
+    apellido1 = VARCHAR(255) NOT NULL,
+    apellido2 = VARCHAR(255) NOT NULL
+    edad = INT(25) NOT NULL
+    edad = INT(25) NOT NULL
+})
+
+execute(query)
+
+
+commit(query)
