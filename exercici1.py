f01eb206ff9ae16f5d1762684a77fd75a1ead3de
Maria Palomeque Guerrero (ext) <maria.palomeque.external@eviden.com>
2024-06-12 16:10:36 +0200

    Exercici 1

---
+++ exercici1.py
@@ -0,0 +1,16 @@
+import PostgreSQl
+
+
+conn = maria_db.connect
+
+database = 'maria_db'
+user = 'maria'
+password = 'lliure'
+host = 'localhost'
+port = ''
+
+cur.connect
+connect.cursor
+
+
+pritn(cur)
